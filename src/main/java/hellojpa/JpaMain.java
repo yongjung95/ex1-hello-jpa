7b167d4f32d8f20808ce52f7c3bab0b4e0d6d0c3
yongjung95 <yongjung95@gmail.com>
2022-05-22 16:34:42 +0900

    2022.05.22 : 김영한 자바 ORM 표준 JPA 프로그래밍 - 기본편
    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,19 +14,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction(); // JPA 의 모든 데이터 변경은 트랜잭션 안에서 해야한다.
         tx.begin();
 
-//        Member member = new Member();
-
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result){
-                System.out.println("member.getName = " + member.getName());
-            }
-
             tx.commit();
         }catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,28 +1,26 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name")
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDateTime createdDate;
+
+    private LocalDateTime lastModifiedDate;
+
+    @Lob
+    private String description;
 }
